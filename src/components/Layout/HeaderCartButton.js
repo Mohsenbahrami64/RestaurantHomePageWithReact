7b44cafdaad80056087a586b4a3aea5d46c086fe
mohsenbahrami64 <mohsenbahrami64@gmail.com>
2023-05-17 11:54:17 +0330

    the secend commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  return (
+    <button className={btnClasses}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
